554c94cd7b7f96d6960ee769c3e90155e1c7a754
John Alexander Quintero Berrío <32532010+jaquinterob@users.noreply.github.com>
2021-10-11 20:07:13 -0500

    Update parte_2.sql

--- solutions/parte_2.sql
+++ solutions/parte_2.sql
@@ -1,41 +1,42 @@
 --1
 SELECT order_date, COUNT(*) AS 'orders_count' 
-from orders
+FROM orders
 GROUP BY order_date
 ORDER by 1
 
 --2
-SELECT a.order_date, SUM(B.list_price) as 'price_order_day' FROM orders a, order_items b  
+SELECT a.order_date, SUM(B.list_price) AS 'price_order_day' 
+FROM orders a, order_items b  
 where  a.order_id = b.order_id
 group by a.order_date
-order by 1
+ORDER BY 1
 
 --3
-SELECT a.order_date,c.product_name,count(*) AS 'sum_day_product'
-from orders a, order_items b, products c
-where a.order_id = b.order_id
+SELECT a.order_date,c.product_name,COUNT(*) AS 'sum_day_product'
+FROM orders a, order_items b, products c
+WHERE a.order_id = b.order_id
 AND b.product_id = c.product_id
 GROUP BY a.order_date,c.product_name
-ORDER by 1
+ORDER BY 1
 
 --4
-SELECT a.order_date,c.product_name,SUM(b.list_price) as 'price_sum'
-from orders a, order_items b, products c
-where a.order_id = b.order_id
+SELECT a.order_date,c.product_name,SUM(b.list_price) AS 'price_sum'
+FROM orders a, order_items b, products c
+WHERE a.order_id = b.order_id
 AND b.product_id = c.product_id
 GROUP BY a.order_date,c.product_name
-ORDER by 1
+ORDER BY 1
 
 --5
-SELECT a.order_date,b.customer_id, count(*) as 'orders_count'
-from orders a, customers b 
-where a.customer_id = b.customer_id
-group by  a.order_date,b.customer_id
-order by 1
+SELECT a.order_date,b.customer_id, COUNT(*) AS 'orders_count'
+FROM orders a, customers b 
+WHERE a.customer_id = b.customer_id
+GROUP BY  a.order_date,b.customer_id
+ORDER BY 1
 
 --6
-SELECT MONTH(a.order_date),b.customer_id, count(*) as 'orders_by_month'
-from orders a, customers b 
-where a.customer_id = b.customer_id
-group by  MONTH(a.order_date),b.customer_id
-order by 1
+SELECT MONTH(a.order_date),b.customer_id, count(*) AS 'orders_by_month'
+FROM orders a, customers b 
+WHERE a.customer_id = b.customer_id
+GROUP BY  MONTH(a.order_date),b.customer_id
+ORDER BY 1
